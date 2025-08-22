b9fd0f70ba0cdda493d6fc2b978f3e85776d0cc4
wahnaton <flightlogscan@flightlogscan.com>
2025-08-21 19:15:17 -0700

    remove more dupes

--- terraform/envs/prod/providers.tf
+++ terraform/envs/prod/providers.tf
@@ -12,18 +12,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
